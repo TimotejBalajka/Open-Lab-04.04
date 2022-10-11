51cdcffbda216485eeb29087dd3961ed51f807f3
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-10-11 09:04:53 +0200

    Update PhoneNumbers.cs

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._04
 {
@@ -6,7 +6,10 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            var phone123 = string.Concat(numbers[0..3]);
+            var phone456 = string.Concat(numbers[3..6]);
+            var phone78910 = string.Concat(numbers[6..]);
+            return new string($"({phone123}) {phone456}-{phone78910}");
         }
     }
 }
